73e9c24fc1c24e5efa9b611944eeaca5a6fd0bcc
mjdibella <74016254+mjdibella@users.noreply.github.com>
2022-01-17 16:26:16 -0800

    Add files via upload

--- Loggly.PSHelper.psm1
+++ Loggly.PSHelper.psm1
@@ -3,23 +3,26 @@ Add-Type -AssemblyName System.Web
 
 function Connect-Loggly {
     param(
-        [Parameter(Mandatory=$true)][string]$tenant
+        [Parameter(Mandatory=$true)][string]$tenant,
+        [Parameter(Mandatory=$true)][string]$apitoken,
+        [Parameter(Mandatory=$true)][string]$logtoken
     )
-    $securedValue = Read-Host -prompt "token" -AsSecureString
-    $bstr = [System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($securedValue)
-    $token = [System.Runtime.InteropServices.Marshal]::PtrToStringAuto($bstr)
     $logglyConfig.tenant = $tenant
-    $logglyConfig.token = $token
+    $logglyConfig.apitoken = $apitoken
+    $logglyConfig.logtoken = $logtoken
     New-ItemProperty -Path $logglyConfig.registryURL -Name tenant -Value $tenant -Force | Out-Null
-    New-ItemProperty -Path $logglyConfig.registryURL -Name token -Value $token -Force | Out-Null
+    New-ItemProperty -Path $logglyConfig.registryURL -Name apitoken -Value $apitoken -Force | Out-Null
+    New-ItemProperty -Path $logglyConfig.registryURL -Name logtoken -Value $logtoken -Force | Out-Null
     Write-host "Connected to Loggly tenant $($logglyConfig.tenant)`n"
 }
 
 function Disconnect-Loggly {
     Remove-ItemProperty -Path $logglyConfig.registryURL -Name tenant | Out-Null
-    Remove-ItemProperty -Path $logglyConfig.registryURL -Name token | Out-Null
+    Remove-ItemProperty -Path $logglyConfig.registryURL -Name apitoken | Out-Null
+    Remove-ItemProperty -Path $logglyConfig.registryURL -Name logtoken | Out-Null
     $logglyconfig.tenant = $null
-    $logglyConfig.token = $null
+    $logglyConfig.apitoken = $null
+    $logglyConfig.logtoken = $null
 }
 
 function Get-LogglyEvent {
@@ -36,7 +39,7 @@ function Get-LogglyEvent {
     #>
     [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
     if (-not $rsid) {
-        $uri = 'https://' + $logglyConfig.tenant + '/apiv2/search?q=' + $query
+        $uri = 'https://' + $logglyConfig.tenant + '.loggly.com/apiv2/search?q=' + $query
         if ($from) {
             $uri = $uri + '&from=' + $from
         }
@@ -51,17 +54,17 @@ function Get-LogglyEvent {
         }
         $uri = [uri]::EscapeUriString($uri)
         try {
-            $webresponse = invoke-webrequest -uri $uri -method Get -headers @{"Authorization" = "bearer $($logglyConfig.token)"}
+            $webresponse = invoke-webrequest -uri $uri -method Get -headers @{"Authorization" = "bearer $($logglyConfig.apitoken)"}
             $response = ConvertFrom-JSON $webresponse.Content
             $rsid = $response.rsid.id
         } catch {
             Set-LogglyRESTErrorResponse
         }
     } 
-    $uri = 'https://' + $logglyConfig.tenant + '/apiv2/events?rsid=' + $rsid
+    $uri = 'https://' + $logglyConfig.tenant + '.loggly.com/apiv2/events?rsid=' + $rsid
     $uri = [uri]::EscapeUriString($uri)
     try {
-        $webresponse = invoke-webrequest -uri $uri -method Get -headers @{"Authorization" = "bearer $($logglyConfig.token)"}
+        $webresponse = invoke-webrequest -uri $uri -method Get -headers @{"Authorization" = "bearer $($logglyConfig.apitoken)"}
         $response = ConvertFrom-JSON $webresponse.Content
         $response.events
     } catch {
@@ -83,7 +86,7 @@ function Get-LogglyEventPage {
     https://<subdomain>.loggly.com/apiv2/events/iterate?next=eea25ee6-0e48-4428-a544-36d6441d132c
     #>
     [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-    $uri = 'https://' + $logglyConfig.tenant + '/apiv2/events/iterate'
+    $uri = 'https://' + $logglyConfig.tenant + '.loggly.com/apiv2/events/iterate'
     if ($query) {
         $uri = $uri + '?q=' + $query
     }
@@ -102,7 +105,7 @@ function Get-LogglyEventPage {
     $uri = [uri]::EscapeUriString($uri)
     while ($uri) {
         try {
-            $webresponse = invoke-webrequest -uri $uri -method Get -headers @{"Authorization" = "bearer $($logglyConfig.token)"}
+            $webresponse = invoke-webrequest -uri $uri -method Get -headers @{"Authorization" = "bearer $($logglyConfig.apitoken)"}
             $response = ConvertFrom-JSON $webresponse.Content
             $response.events
             $uri = $response.next
@@ -126,7 +129,7 @@ function Get-LogglyEventCount {
     $ curl -H 'Authorization: bearer <token>' -XGET 'https://<subdomain>.loggly.com/apiv2/events/count?q=*&from=-10m&until=now'
     #>
     [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-    $uri = 'https://' + $logglyConfig.tenant + '/apiv2/events/count'
+    $uri = 'https://' + $logglyConfig.tenant + '.loggly.com/apiv2/events/count'
     if ($query) {
         $uri = $uri + '?q=' + $query
     }
@@ -138,7 +141,7 @@ function Get-LogglyEventCount {
     }
     $uri = [uri]::EscapeUriString($uri)
     try {
-        $webresponse = invoke-webrequest -uri $uri -method Get -headers @{"Authorization" = "bearer $($logglyConfig.token)"}
+        $webresponse = invoke-webrequest -uri $uri -method Get -headers @{"Authorization" = "bearer $($logglyConfig.apitoken)"}
         $response = ConvertFrom-JSON $webresponse.Content
         $response
     } catch {
@@ -159,7 +162,7 @@ function Get-LogglyEventMetric {
     $ curl -H 'Authorization: bearer <token>' -XGET 'https://<subdomain>.loggly.com/apiv2/volume-metrics?measurement_types=count&from=2018-12-20T21%3A24%3A18.007Zh&until=2018-12-21T21%3A24%3A18.007Z&host=customer_host'
     #>
     [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-    $uri = 'https://' + $logglyConfig.tenant + '/apiv2/volume-metrics'
+    $uri = 'https://' + $logglyConfig.tenant + '.loggly.com/apiv2/volume-metrics'
     if ($from) {
         $uri = $uri + '?from=' + $from
     }
@@ -184,7 +187,7 @@ function Get-LogglyEventMetric {
     }
     $uri = [uri]::EscapeUriString($uri)
     try {
-        $webresponse = invoke-webrequest -uri $uri -method Get -headers @{"Authorization" = "bearer $($logglyConfig.token)"}
+        $webresponse = invoke-webrequest -uri $uri -method Get -headers @{"Authorization" = "bearer $($logglyConfig.apitoken)"}
         $response = ConvertFrom-JSON $webresponse.Content
         $response
     } catch {
@@ -204,7 +207,7 @@ function Find-LogglyEvent {
     $ curl -H 'Authorization: bearer <token>' "https://<subdomain>.loggly.com/apiv2/search?q=*&from=-2h&until=now&size=10"
     #>
     [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-    $uri = 'https://' + $logglyConfig.tenant + '/apiv2/search?q=' + $query
+    $uri = 'https://' + $logglyConfig.tenant + '.loggly.com/apiv2/search?q=' + $query
     if ($from) {
         $uri = $uri + '&from=' + $from
     }
@@ -219,7 +222,7 @@ function Find-LogglyEvent {
     }
     $uri = [uri]::EscapeUriString($uri)
     try {
-        $webresponse = invoke-webrequest -uri $uri -method Get -headers @{"Authorization" = "bearer $($logglyConfig.token)"}
+        $webresponse = invoke-webrequest -uri $uri -method Get -headers @{"Authorization" = "bearer $($logglyConfig.apitoken)"}
         $response = ConvertFrom-JSON $webresponse.Content
         $response.rsid
     } catch {
@@ -227,16 +230,64 @@ function Find-LogglyEvent {
     }
 }
 
+function Send-LogglyEvent {
+    [cmdletbinding()]
+    param(
+        [Parameter(ValueFromPipeline,parameterSetName="fromPipeline")][PSObject[]]$messageObjects,
+        [Parameter(Mandatory=$true,parameterSetName="byParam")][string]$message
+    )
+    <#
+    $ curl -H "content-type:text/plain" -d 'Hello' https://logs-01.loggly.com/inputs/TOKEN/tag/http/
+    $ curl -H "content-type:application/json" -d "{\"message\":\"hello world\", \"from\":\"hoover\"}" https://logs-01.loggly.com/inputs/TOKEN/tag/http/
+    #>
+    begin {
+        [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+        $uri = 'https://logs-01.loggly.com/inputs/' + $logglyConfig.logtoken + '/tag/http/'
+    }
+    process {
+        if ($_) {
+            foreach ($messageObject in $messageObjects) { 
+                try {
+                    $body = ConvertTo-Json $messageObject
+                    $webResponse = Invoke-WebRequest -uri $uri -Method Post -body $body -ContentType application/json
+                    $response = ConvertFrom-JSON $webResponse.Content
+                    $response | add-member -NotePropertyName message -NotePropertyValue $messageObject.message
+                    $response
+                } catch {
+                    Set-LogglyRESTErrorResponse
+                }
+            } 
+        } else {
+            if ($message) {
+                try {
+                    $webResponse = Invoke-WebRequest -uri $uri -Method Post -body $message -ContentType text/plain
+                    $response = ConvertFrom-JSON $webResponse.Content
+                    $response | add-member -NotePropertyName message -NotePropertyValue $message
+                    $response
+                } catch {
+                    Set-LogglyRESTErrorResponse
+                }
+            }
+        }
+    }
+    end {
+    }
+}
+
 function Set-LogglyRESTErrorResponse {
-    $logglyRESTErrorResponse.tenant = $logglyConfig.tenant
-    $logglyRESTErrorResponse.token = $logglyConfig.token
-    $logglyRESTErrorResponse.statusCode = $_.Exception.Response.StatusCode
-    $logglyRESTErrorResponse.statusDescription = $_.Exception.Response.StatusDescription
-    $result = $_.Exception.Response.GetResponseStream()
-    $reader = New-Object System.IO.StreamReader($result)
-    $logglyRESTErrorResponse.responseBody = $reader.ReadToEnd();
-    $logglyRESTErrorResponse.responseError = (ConvertFrom-JSON $logglyRESTErrorResponse.responsebody).message
-    $logglyRESTErrorResponse
+    if ($_.Exception.Response) {
+        $logglyRESTErrorResponse.tenant = $logglyConfig.tenant
+        $logglyRESTErrorResponse.apitoken = $logglyConfig.apitoken
+        $logglyRESTErrorResponse.statusCode = $_.Exception.Response.StatusCode
+        $logglyRESTErrorResponse.statusDescription = $_.Exception.Response.StatusDescription
+        $result = $_.Exception.Response.GetResponseStream()
+        $reader = New-Object System.IO.StreamReader($result)
+        $logglyRESTErrorResponse.responseBody = $reader.ReadToEnd();
+        $logglyRESTErrorResponse.responseError = (ConvertFrom-JSON $logglyRESTErrorResponse.responsebody).message
+        $logglyRESTErrorResponse
+    } else {
+        write-error $_
+    }
     break
 }
 
@@ -279,7 +330,8 @@ $logglyConfig = [ordered]@{
 New-Variable -Name logglyconfig -Value $logglyConfig -Scope script -Force
 $logglyRESTErrorResponse = [ordered]@{
     tenant = $null
-    token = $null
+    apiToken = $null
+    logToken = $null
     statusCode = $null
     statusDescription = $null
     responseBody = $null
@@ -291,7 +343,8 @@ if ($registryKey -eq $null) {
     Write-Warning "Autoconnect failed.  API key not found in registry.  Use Connect-Loggly to connect manually."
 } else {
     $logglyconfig.tenant = $registryKey.tenant
-    $logglyConfig.token = $registryKey.token
+    $logglyConfig.apiToken = $registryKey.apitoken
+    $logglyConfig.logToken = $registryKey.logtoken
     Write-host "Connected to Loggly tenant $($registryKey.tenant)`n"
 }
 Write-host "Cmdlets added:`n$(Get-Command | where {$_.ModuleName -eq 'Loggly.PSHelper'})`n"
